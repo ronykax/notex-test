50d78c574c22e53438c5e2d17b78d3905c4f61ba
Rony Kati <ronykax@proton.me>
2024-05-31 13:33:31 +0000

    make webhook private

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,7 +1,7 @@
-import { DISCORD_WEBHOOK_URL } from "$env/static/private"
+import { PRIVATE_DISCORD_WEBHOOK_URL } from "$env/static/private"
 
 export async function load() {
     return {
-        webhookURL: DISCORD_WEBHOOK_URL
+        webhookURL: PRIVATE_DISCORD_WEBHOOK_URL
     }
 }
